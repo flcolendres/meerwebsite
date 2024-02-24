9bd6604634c6179fc65293f56c609805b5da8ff7
flcolendres <flcolendres@myseneca.ca>
2024-02-24 13:23:21 -0500

    removed template

---
+++ app/index.js
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Toronto Driving Lesson</title>
+      </Head>
+      Hello World
+    </>
+  );
+}
