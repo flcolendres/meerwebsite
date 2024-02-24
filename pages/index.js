a7fc6acb981a5c795c910bd258c1f329f4f52216
flcolendres <flcolendres@myseneca.ca>
2024-02-24 14:46:33 -0500

    Added basic info and background color

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
 import Head from "next/head";
-import styles from "@/app/page.module.css"
+import styles from "../styles/page.module.css"
 import { Col, Container, Nav, Navbar, Row, Image } from "react-bootstrap";
 
 export default function Home() {
@@ -21,13 +20,18 @@ export default function Home() {
         </Navbar.Collapse>
       </Container>
       </Navbar>
+
       <main className={`${styles.main}`}>
           <Container>
             <Row>
-              <Col>
-                <Image src="assets/car.png" fluid/>
+              <Col xs={6} md={4}>
+                <Image className={`${styles.image}`} src="/assets/car.png" alt="Car image" fluid/>
               </Col>
-              <Col>Welcome to Meer's Driving School, where expertise meets experience on the road to safe and confident driving. I'm Meer, your dedicated driving instructor with a passion for empowering learners with the skills and knowledge needed to navigate the roads with confidence. With years of experience in the driver's seat, I am committed to providing top-notch instruction tailored to your individual needs. Join me on this journey towards mastering the art of driving and unlocking a world of possibilities behind the wheel.</Col>
+              <Col>
+                Hello! My name is Meer, your dedicated driving instructor with a passion for empowering learners with the skills and knowledge needed to navigate the roads with confidence. 
+                With years of experience in the driver's seat, I am committed to providing top-notch instruction tailored to your individual needs. 
+                Join me on this journey towards mastering the art of driving and unlocking a world of possibilities behind the wheel.
+              </Col>
             </Row>
           </Container>
         
