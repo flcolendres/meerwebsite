319529574e89abceb3c34fa4a7de96561a0b6a77
flcolendres <flcolendres@myseneca.ca>
2024-02-27 01:49:21 -0500

    added images

--- pages/index.js
+++ pages/index.js
@@ -3,17 +3,22 @@ import styles from "../styles/page.module.css"
 import { Col, Container, Nav, Navbar, Row, Image, Card, Alert } from "react-bootstrap";
 
 export default function Home() {
+  const email = "waiselham@hotmail.ca"
+  const phone = "647-303-2773"
   function showMessage(){
     // <Alert variant="success">
     //   <Alert.Heading>You can contact me via:</Alert.Heading>
     // </Alert>
-    alert("E-mail: waiselham@hotmail.ca\nPhone: 647-303-2773")
+    alert(`E-mail: ${email}\nPhone: ${phone}`)
     console.log("pressed")
   }
   return (
     <>
       <Head>
         <title>Toronto Driving Lesson</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet"></link>
       </Head>
       <Navbar>
       <Container>
@@ -29,47 +34,52 @@ export default function Home() {
       </Navbar>
 
       <main className={`${styles.main}`}>
-          <Container>
-            <Row>
-              <Col xs={6} md={4}>
-                <Image className={`${styles.image}`} src="/assets/car.png" alt="Car image"/>
-              </Col>
-              <Col className={`${styles.column}`}>
-                Hello! My name is Meer, your dedicated driving instructor with a passion for empowering learners with the skills and knowledge needed to navigate the roads with confidence. 
-                With years of experience in the driver's seat, I am committed to providing top-notch instruction tailored to your individual needs. 
-                Join me on this journey towards mastering the art of driving and unlocking a world of possibilities behind the wheel.
-              </Col>
-            </Row>
-          </Container>
-          <br />
-          <Container>
-            <Row className={`${styles.card}`}>
-              <Col xs={6} md={4}>
-                <Card style={{ width: '18rem'}}>
-                  <Card.Img variant="top" src="/assets/g2.png" />
-                  <Card.Body>
-                    <Card.Title>G2 Lesson Price</Card.Title>
-                    <Card.Text>
-                      $50/hr
-                    </Card.Text>
-                  </Card.Body>
-                </Card>
-              </Col>
-              <Col xs={6} md={4}> 
-                <Card style={{ width: '18rem' }}>
-                  <Card.Img variant="top" src="/assets/g.png" />
-                  <Card.Body>
-                    <Card.Title>G Lesson Price</Card.Title>
-                    <Card.Text>
-                      $60/hr
-                    </Card.Text>
-                  </Card.Body>
-                </Card>
-              </Col>
-            </Row>
-          </Container>
+        <Container style={{position:'relative', textAlign:'center', color:'white'}}>
+          <Image src="/assets/highway.jpg" style={{maxWidth:'100%', borderRadius:'50%'}}/>
+          <div className={'textA'}>Looking for a driving lesson?</div>
+          <div  id="textB" style={{position:"absolute", top:"50%", left:"50%", transform:"translate(-50%, -50%)", fontSize:"10px"}}>
+            <mark>Email: {email}</mark><br /> 
+            <mark>Phone: {phone} </mark>
+            
+          </div>
+        </Container>
+        {/* <Container fluid="md">
+        <h3 xs="auto" style={{maxWidth: '100%', marginTop: '10rem', textAlign:'center'}}> 
+          Hello! My name is Meer, your dedicated driving instructor with a passion for empowering learners 
+          with the skills and knowledge needed to navigate the roads with confidence.
+          I am committed to providing top-notch instruction tailored to your individual needs. 
+          Join me on this journey towards mastering the art of driving and unlocking a world of possibilities behind the wheel.
+        </h3>
+        </Container> */}
+
+        <Container fluid style={{marginTop:"5rem"}}>
+          <Row>
+            <Col xs="auto">
+              <Card style={{ width: '15rem' }}>
+                <Card.Img variant="top" src="/assets/g2.png" />
+                <Card.Body>
+                  <Card.Title>G2 Lesson Price</Card.Title>
+                  <Card.Text>
+                    $50/hr
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col xs="auto">
+              <Card style={{ width: '15rem' }}>
+                <Card.Img variant="top" src="/assets/g.png" />
+                <Card.Body>
+                  <Card.Title>G Lesson Price</Card.Title>
+                  <Card.Text>
+                    $60/hr
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+        </Container>
           
-          <Image className={`${styles.img}`}src="https://www.autos.ca/wp-content/uploads/2013/06/478204361.jpg"></Image>
+          {/* <Image className={`${styles.img}`}src="https://www.autos.ca/wp-content/uploads/2013/06/478204361.jpg"></Image> */}
       </main>
 
 
